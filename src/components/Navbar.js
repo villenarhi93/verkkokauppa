c52905a156201a7f39c1999e105375a860801bf0
Ville Närhi <villenarhi@hotmail.fi>
2022-11-07 19:13:03 +0200

    changed name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ export default function Navbar() {
                 <form class="d-flex">
                     <button class="btn btn-outline-dark" type="submit">
                         <i class="bi-cart-fill me-1"></i>
-                        Cart
+                        Ostoskori
                         <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
                     </button>
                 </form>
