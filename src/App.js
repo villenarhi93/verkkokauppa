a717b122e84a99b6ebb74f3f8dfaaf74aa2a7c34
Ville Närhi <villenarhi@hotmail.fi>
2022-11-07 18:57:22 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 
 function App() {
