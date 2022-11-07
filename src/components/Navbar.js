e1333b681b16de13e0724502e5d00c03cbb0bb0f
Ville Närhi <villenarhi@hotmail.fi>
2022-11-07 19:12:22 +0200

    changed names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,15 +9,18 @@ export default function Navbar() {
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Etusivu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Tietoja</a></li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Kauppa</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <li><a class="dropdown-item" href="#!">Kaikki tuotteet</a></li>
                             <li><hr class="dropdown-divider" /></li>
-                            <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                            <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            <li><a class="dropdown-item" href="#!">Tuoteryhmä 1</a></li>
+                            <li><a class="dropdown-item" href="#!">Tuoteryhmä 2</a></li>
+                            <li><a class="dropdown-item" href="#!">Tuoteryhmä 3</a></li>
+                            <li><a class="dropdown-item" href="#!">Tuoteryhmä 4</a></li>
+                            <li><a class="dropdown-item" href="#!">Tuoteryhmä 5</a></li>
                         </ul>
                     </li>
                 </ul>
