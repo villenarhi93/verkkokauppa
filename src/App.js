1dbf64567300d013fe4bcc08aa33a2925a752fcc
Ville Närhi <villenarhi@hotmail.fi>
2022-11-07 18:42:21 +0200

    header imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   );
 
