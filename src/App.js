0ac228962848d39fcc124a1ae39dadc490138088
Ville Närhi <villenarhi@hotmail.fi>
2022-11-07 19:06:02 +0200

    added Main js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from './components/Main';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar />
     <Header />
+    <Main />
     <Footer />
     </>
   );
