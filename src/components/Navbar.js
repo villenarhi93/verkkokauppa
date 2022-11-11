f8bfdde326dad7a2c5a464ae816de5fbfcdb9af6
Ville Närhi <villenarhi@hotmail.fi>
2022-11-11 17:01:19 +0200

    more links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
                 <form class="d-flex">
                     <button class="btn btn-outline-dark" type="submit">
                         <i class="bi-cart-fill me-1"></i>
-                        Ostoskori
+                        <Link className="nav-link" to="/cart">Ostoskori</Link>
                         <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
                     </button>
                 </form>
