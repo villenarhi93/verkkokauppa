eea2ecc63ac361aecb4dd95c09df17cccb4ce99a
Ville Närhi <villenarhi@hotmail.fi>
2022-11-11 17:04:57 +0200

    added cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default function Cart () {
+    return (
+    <>
+    
+    </>
+    )
+}
