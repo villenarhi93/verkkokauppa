41183c2d3deed19a0bf0cbcf24bdc7e6a2ffffa0
Ville Närhi <villenarhi@hotmail.fi>
2022-11-11 16:54:47 +0200

    addec browserouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 
 
 function App() {
