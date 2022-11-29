a1ab79e65525f08bee388b9e8291ce5ae5978079
tuomasesp <101732066+tuomasesp@users.noreply.github.com>
2022-11-29 20:55:47 +0200

    Home.js bootstrapped

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,40 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header class="bg-dark py-1">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Living Tech</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">The Best You Can Get</p>
-                    <div class="container-lg my-3">
-                        <div id="myCarousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+        <header className="bg-dark py-1">
+            <div className="container px-4 px-lg-5 my-5">
+                <div className="text-center text-white">
+                    <h1 className="display-4 fw-bolder">Living Tech</h1>
+                    <p className="lead fw-normal text-white-50 mb-0">The Best You Can Get</p>
+                    <div className="container-lg my-3">
+                        <div id="myCarousel" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                            <ol className="carousel-indicators">
+                                <li data-bs-target="#myCarousel" data-bs-slide-to="0" className="active"></li>
                                 <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
                                 <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
                             </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="./images/pexels-pixabay-38568.jpg" class="w-50" alt="Slide 1"></img>
-                                    <div class="carousel-caption d-none d-md-block">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="./images/pexels-pixabay-38568.jpg" className="w-50" alt="Slide 1"></img>
+                                    <div className="carousel-caption d-none d-md-block">
                                     </div>
                                 </div>
-                                <div class="carousel-item">
-                                    <img src="./images/pexels-pixabay-414548.jpg" class="w-50" alt="Slide 2"></img>
-                                    <div class="carousel-caption d-none d-md-block">
+                                <div className="carousel-item">
+                                    <img src="./images/pexels-pixabay-414548.jpg" className="w-50" alt="Slide 2"></img>
+                                    <div className="carousel-caption d-none d-md-block">
                                     </div>
                                 </div>
-                                <div class="carousel-item">
-                                    <img src="./images/pexels-roberto-nickson-3082341.jpg" class="w-50" alt="Slide 3"></img>
-                                    <div class="carousel-caption d-none d-md-block">
+                                <div className="carousel-item">
+                                    <img src="./images/pexels-roberto-nickson-3082341.jpg" className="w-50" alt="Slide 3"></img>
+                                    <div className="carousel-caption d-none d-md-block">
                                     </div>
                                 </div>
                             </div>
-                            <a class="carousel-control-prev" href="#myCarousel" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon"></span>
+                            <a className="carousel-control-prev" href="#myCarousel" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon"></span>
                             </a>
-                            <a class="carousel-control-next" href="#myCarousel" data-bs-slide="next">
-                                <span class="carousel-control-next-icon"></span>
+                            <a className="carousel-control-next" href="#myCarousel" data-bs-slide="next">
+                                <span className="carousel-control-next-icon"></span>
                             </a>
                         </div>
                     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,10 @@ function executeSearch(e) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container px-4 px-lg-5">
-            <a className="navbar-brand" href="#!">Verkkokauppa</a>
+            <div>
+                <img src="./images/logo.png" className="logo" alt="Living Tech logo"></img>
+            </div>
+            <a className="navbar-brand" href="#!"></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -44,8 +47,7 @@ function executeSearch(e) {
                     <li className="nav-item"><a className="nav-link" href="#!"></a>
                         <Link className="nav-link" to="/about">Tietoja</Link>
                     </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Kauppa</a>
+                    <li className="nav-item dropdown"><a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">Tuotteet</a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                             {categories.map(category => (
                             <li>
