ec7624c2933bf76800a86a1e374a5ec555b6b184
Ville Närhi <villenarhi@hotmail.fi>
2022-12-19 02:37:06 +0200

    sivut lisätty

---
+++ src/pages/ThankFeedback.js
@@ -0,0 +1,8 @@
+export default function ThankFeedback() {
+    return (
+        <>
+        <h5>Kiitos palautteestasi! Vastaamme sinulle mahdollisimman pian.</h5>
+        <br/>
+        </>
+    )
+}
---
+++ src/pages/ThankOrder.js
@@ -0,0 +1,9 @@
+export default function ThankOrder() {
+    return (
+        <>
+        <h5>Kiitos tilauksestasi! Saat pian tilausvahvistuksen sähköpostiisi.</h5>
+        <div>Mikäli sinulle herää jotain kysyttävää tilaukseesi liittyen, olethan yhteydessä asiakaspalveluumme.</div>
+        <br/>
+        </>
+    )
+}
